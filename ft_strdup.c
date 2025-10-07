6f7cdeb533216a1c72f45891e2f053517fd2a72a
Chloe Mauley <cmauley@c1r4s1.42lausanne.ch>
2025-10-07 18:07:12 +0200

    prout

---
+++ ft_strdup.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmauley <cmauley@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 23:13:43 by cmauley           #+#    #+#             */
+/*   Updated: 2025/10/06 23:36:40 by cmauley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+
+	dest = (char *) malloc(ft_strlen(src) + 1);
+	if (!dest)
+		return (NULL);
+	ft_memcpy(dest, src, ft_strlen(src) + 1);
+	return (dest);
+}
+/*int	main(void)
+{
+	char	*src = "Hello World";
+	char	*copy;
+
+	copy = ft_strdup(src);
+	printf("Source = %s\n", src);
+	printf("Copie = %s\n", copy);
+
+	free(copy);
+	return (0);
+}*/
