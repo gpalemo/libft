d3e418edce6f59547daad73410f30c744e23b54a
gpalemo <chloemauley@gmail.com>
2025-10-11 22:36:44 +0200

    prout

--- ft_split.c
+++ ft_split.c
@@ -6,16 +6,11 @@
 /*   By: cmauley <cmauley@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 20:32:37 by cmauley           #+#    #+#             */
-/*   Updated: 2025/10/08 21:26:26 by cmauley          ###   ########.fr       */
+/*   Updated: 2025/10/11 22:23:22 by cmauley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	**ft_split(char const *s, char c)
-{
-
-}
 
 static int	word_count(const char *s, char c)
 {
@@ -30,12 +25,63 @@ static int	word_count(const char *s, char c)
 			z++;
 		y++;
 	}
+	if (z == 0)
+		return (-1);
 	return (z + 1);
 }
 
-static char	*word_dup(const char *s, int start, int end)
+/*static void	free_all(char **tab, int i)
 {
-	truc dans le genre : return (ft_substr(s, start, end - start));
+	while (i > 0)
+	{
+		i--;
+		free (tab[i]);
+	}
+	free (tab);
+	return (NULL);
+}*/
+
+char	**ft_split(char const *s, char c)
+{
+	char	**str;
+	int		w;
+	int		i;
+	int		start;
+	
+	if (!s)
+		return (NULL);
+	str = malloc((word_count(s, c) + 1) * sizeof(char *));
+	if (!str)
+		return (NULL);
+	w = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (!s[i])
+			break;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		str[w] = ft_substr(s, start, i - start);
+		w++;
+	}
+	str[w] = '\0';
+	return (str);
 }
+/*int	main()
+{
+	char	**tab;
+	int		i;
 
-
+	tab = ft_split("caca pipi caca  ", ' ');
+	i = 0;
+	while (tab[i])
+	{
+		printf("str = %d : %s\n", i, tab[i]);
+		i++;
+	}
+	write
+	return (0);
+}*/
